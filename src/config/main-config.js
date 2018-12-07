ab19781928ea87716c459696eb5772ee541c136e
Evgheni Curdov <kurdov.e@gmail.com>
2018-12-07 10:18:50 -0800

    sendgrid debugging

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const session = require("express-session");
 const flash = require("express-flash");
@@ -10,6 +11,7 @@ module.exports = {
   init(app, express){
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, "..", "assets")));
     app.use(expressValidator());
     app.use(session({
